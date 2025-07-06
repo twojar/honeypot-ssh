31c91c1bc0fe7bb7d664a7e844b9dca644ebf83a
alan <alanbenpitcher@gmail.com>
2025-07-06 19:39:59 -0400

    added http honeypot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # LIBRARIES
 import argparse
 from honeypotSSH import *
+from honeypotHTTP import *
 import time
 # PARSE ARGUMENTS
 if __name__ == "__main__":
@@ -15,14 +16,23 @@ if __name__ == "__main__":
 
     #store args
     args = parser.parse_args()
-
     try:
         if args.ssh:
             print("Running SSH Honeypot...")
             honeypot(args.address, args.port, args.username, args.password)
+            if not args.username:
+                username = None
+            if not args.password:
+                password = None
         elif args.http:
-            print("Running HTTP Honeypot")
-            pass
+            print("Running HTTP Honeypot...")
+            honeypot_web()
+            if not args.username:
+                args.username = "admin"
+            if not args.password:
+                password = "password"
+            print(f"Port: {args.port} Username: {args.username} Password: {args.password}")
+            run_honeypot_web(args.port, args.username, args.password)
         else:
             print("ERROR: Choose a honeypot to run!")
             print("SSH (--ssh) or HTTP (--http)")
